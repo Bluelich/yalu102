d4ec085fd29e3a7dec1a9c9b7112aceec442784c
Bluelich <bluelich@qq.com>
2019-08-29 22:15:48 +0800

    update system -> gsystem

--- yalu102/jailbreak.m
+++ yalu102/jailbreak.m
@@ -94,6 +94,7 @@ uint64_t WriteAnywhere32(uint64_t addr, uint32_t val) {
 }
 
 #import "pte_stuff.h"
+extern int (*gsystem)(const char *);
 
 void exploit(void* btn, mach_port_t pt, uint64_t kernbase, uint64_t allprocs)
 {
@@ -880,18 +881,18 @@ remappage[remapcnt++] = (x & (~PMK));\
                 open("/.cydia_no_stash",O_RDWR|O_CREAT);
                 
                 
-                system("echo '127.0.0.1 iphonesubmissions.apple.com' >> /etc/hosts");
-                system("echo '127.0.0.1 radarsubmissions.apple.com' >> /etc/hosts");
+                gsystem("echo '127.0.0.1 iphonesubmissions.apple.com' >> /etc/hosts");
+                gsystem("echo '127.0.0.1 radarsubmissions.apple.com' >> /etc/hosts");
                 
-                system("/usr/bin/uicache");
+                gsystem("/usr/bin/uicache");
                 
-                system("killall -SIGSTOP cfprefsd");
+                gsystem("killall -SIGSTOP cfprefsd");
                 NSMutableDictionary* md = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/com.apple.springboard.plist"];
                 
                 [md setObject:[NSNumber numberWithBool:YES] forKey:@"SBShowNonDefaultSystemApps"];
                 
                 [md writeToFile:@"/var/mobile/Library/Preferences/com.apple.springboard.plist" atomically:YES];
-                system("killall -9 cfprefsd");
+                gsystem("killall -9 cfprefsd");
                 
             }
             {
@@ -928,8 +929,8 @@ remappage[remapcnt++] = (x & (~PMK));\
     chmod("/private/var/mobile", 0777);
     chmod("/private/var/mobile/Library", 0777);
     chmod("/private/var/mobile/Library/Preferences", 0777);
-    system("rm -rf /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; touch /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; chmod 000 /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; chown 0:0 /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate");
-    system("(echo 'really jailbroken'; /bin/launchctl load /Library/LaunchDaemons/0.reload.plist)&");
+    gsystem("rm -rf /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; touch /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; chmod 000 /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; chown 0:0 /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate");
+    gsystem("(echo 'really jailbroken'; /bin/launchctl load /Library/LaunchDaemons/0.reload.plist)&");
     WriteAnywhere64(bsd_task+0x100, orig_cred);
     sleep(2);
     
